5a15d14c931b0387e5032440aeaf5f14d306110c
penapple1 <>
2021-05-22 02:01:38 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const btnStartRecord = document.getElementById('btnStartRecord');
+const btnStopRecord = document.getElementById('btnStopRecord');
+const btnPlayText = document.getElementById('playText');  
+const text = document.getElementById('text');
+
+let recognition = new webkitSpeechRecognition();
+
+recognition.lang = 'en-US' 
+recognition.continuous = true;
+recognition.interimResults = false; 
+
+recognition.onresult = (event) => {
+  const results = event.results;
+  const sentence = results[results.length - 1][0].transcript;
+  text.value += sentence;
+} 
+
+recognition.onend = (event) => {
+  console.log('the microphone stops listening');
+}
